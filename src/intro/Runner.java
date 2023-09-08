9be6601ee63550b56e1b09f9cd449cdea4dc55ed
jharry444 <jharrison1@qa.com>
2023-09-08 09:46:02 +0100

    modified the runner

--- src/intro/Runner.java
+++ src/intro/Runner.java
@@ -5,7 +5,7 @@ public class Runner {
     // generate using psvm
     public static void main(String[] args) {
         // start writing code here
-        // generate using sout
+        // generated using sout
         // auto-complete using ctrl + space
         System.out.println("Hello, World!");
     }
