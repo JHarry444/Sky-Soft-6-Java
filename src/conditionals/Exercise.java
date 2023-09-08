6fa5ca69b7bcd465c461cc6944f4bf31677d4b2e
jharry444 <jharrison1@qa.com>
2023-09-08 17:04:10 +0100

    solution for switch question

--- src/conditionals/Exercise.java
+++ src/conditionals/Exercise.java
@@ -30,5 +30,33 @@ public class Exercise {
         if (colour.equals("green")) System.out.println("GO!");
         else if (colour.equals("amber")) System.out.println("Prep");
         else if (colour.equals("red")) System.out.println("Wait");
+
+        System.out.println("Enter a number from 1 - 12:");
+        String month = scan.nextLine();
+
+        switch (month) {
+            case "12":
+            case "1":
+            case "2":
+                System.out.println("It's winter");
+                break;
+            case "3":
+            case "4":
+            case "5":
+                System.out.println("It's spring");
+                break;
+            case "6":
+            case "7":
+            case "8":
+                System.out.println("It's summer");
+                break;
+            case "9":
+            case "10":
+            case "11":
+                System.out.println("It's autumn");
+                break;
+            default:
+                System.out.println("That ain't a month, bruh.");
+        }
     }
 }
