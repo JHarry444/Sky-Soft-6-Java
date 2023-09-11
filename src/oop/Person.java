fdc8dc56e7c74ec8ad2ecd0e629e52b21a636766
jharry444 <jharrison1@qa.com>
2023-09-11 12:00:45 +0100

    constructors and static

--- src/oop/Person.java
+++ src/oop/Person.java
@@ -13,10 +13,55 @@ public class Person {
 
     private String hairColour;
 
+    private static String latinName = "Homo Sapien";
+
+    // No return type?
+    // constructs a new instance of Person
+    // default constructor
+    public Person() {
+
+    }
+
+//    public Person(String name, String job, int age) {
+//        this.name = name;
+//        this.age = age;
+//        this.job = job;
+//    }
+//  Can NOT have two methods with the same 'signature' -> same types in same order
+    public Person(String job, String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.job = job;
+    }
+
+
+    public Person(String name, int age, String job) {
+        this.name = name;
+        this.age = age;
+        this.job = job;
+    }
+
+    // overloaded constructor -> same name but different parameters
+    public Person(String name, int age, String jobTitle, String hairColour) {
+        this.name = name;
+        this.age = age;
+        // parameter CAN have a different name but doesn't usually
+        job = jobTitle;
+        this.hairColour = hairColour;
+    }
+
     public String introduction() {
         return "Hello, my name is " + name + " and I am a " + age + " years old " + job + " with " + hairColour + " hair";
     }
 
+    public static void setLatinName(String latinName) {
+        Person.latinName = latinName;
+    }
+
+    // cannot access non-static variables
+    public static String getLatinName() {
+        return latinName;
+    }
     public String getName() {
         return name;
     }
--- src/oop/Runner.java
+++ src/oop/Runner.java
@@ -1,5 +1,7 @@
 package oop;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Scanner;
 
 public class Runner {
@@ -8,13 +10,27 @@ public class Runner {
         Person me = new Person(); // creates an object of type Person
         // dot syntax -> access the members of an objects
         System.out.println(me.introduction());
-
         me.setName("Jordan");
-        me.setAge(-29);
+        me.setAge(29);
         me.setJob("Trainer");
         me.setHairColour("Black");
 
         System.out.println(me.introduction());
+
+
+        Person cameron =  new Person("Cameron", 30, "Trainer", "Brown");
+        System.out.println(cameron.introduction());
+
+        // bad mojo
+        System.out.println(me.getLatinName());
+        System.out.println(cameron.getLatinName());
+
+        // correct way to access a static variable
+        Person.setLatinName("Homo Superior");
+
+        // bad mojo
+        System.out.println(me.getLatinName());
+        System.out.println(cameron.getLatinName());
 
     }
 }
