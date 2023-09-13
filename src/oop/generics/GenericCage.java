b00043d60deda09e81d74a88c324d5eb36f02a66
jharry444 <jharrison1@qa.com>
2023-09-13 11:09:25 +0100

    generics example

--- src/oop/generics/GenericCage.java
+++ src/oop/generics/GenericCage.java
@@ -2,14 +2,19 @@ package oop.generics;
 
 import oop.further.Animal;
 
-public class GenericCage<T extends Animal> {
+// Generics create a placeholder that you can use instead of the actual type
+// This lets the user set the type when they instantiate the class
+public class GenericCage<T extends Animal> { // cage will work with any type as long as it's an animal
 
+    // works with attribute type
     private T animal;
 
+    // return types
     public T getAnimal() {
         return animal;
     }
 
+//    and parameter types
     public void setAnimal(T animal) {
         this.animal = animal;
     }
